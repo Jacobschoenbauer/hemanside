b4d0e76dfcf7015f34625f64a683e028a645f960
Jacobschoenbauer <jhwt65@gmail.com>
2022-09-09 15:45:44 -0500

    added fun javascript

--- asset/script.js
+++ asset/script.js
@@ -1,11 +1,22 @@
-var powCharter = ["He-man", "Skeletor", "She-ra", "Hordak", "King Hiss" ]
+var powCharter = [
+  "He-man",
+  "Skeletor",
+  "She-ra",
+  "Hordak",
+  "King Hiss",
+  "Optimus",
+];
 
+if (powCharter[0] === "He-man")
+  console.log("He-man is not my favorite charecter");
+powCharter.forEach(function (charecter) {
+  console.log(charecter);
+  if (charecter === "He-man") {
+    console.log("He-man is my favorite charecter");
+  } else {
+    console.log("He-man was defeated by " + charecter);
+  }
+});
 
 console.log(powCharter[0]);
 console.log(powCharter[4]);
-
-
-
-
-
-
