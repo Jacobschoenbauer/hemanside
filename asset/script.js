280713ce34338938a51ca6623204bb3925dbff9e
Jacobschoenbauer <jhwt65@gmail.com>
2022-09-11 09:01:59 -0500

    cleaned up javascript

--- asset/script.js
+++ asset/script.js
@@ -7,10 +7,10 @@ var powCharter = [
   "Optimus",
 ];
 
-if (powCharter[0] === "He-man")
-  console.log("He-man is not my favorite charecter");
+
 powCharter.forEach(function (charecter) {
   console.log(charecter);
+
   if (charecter === "He-man") {
     console.log("He-man is my favorite charecter");
   } else {
@@ -18,5 +18,3 @@ powCharter.forEach(function (charecter) {
   }
 });
 
-console.log(powCharter[0]);
-console.log(powCharter[4]);
